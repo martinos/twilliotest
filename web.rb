0c2d5ca3dbdcc83fa98b55c0499018175eff2bc1
Martin Chabot <chabotm@gmail.com>
2012-06-27 02:21:38 -0400

    Adding basic response

--- web.rb
+++ web.rb
@@ -2,9 +2,15 @@ require "rubygems"
 require "bundler/setup"
 
 require 'sinatra'
+require 'builder'
 
 post '/' do
-  request.body.read
+  builder do |xml|
+    xml.instruct!
+    xml.Response do |xml|
+      xml.Say "This is a test" 
+    end
+  end
 end
 
 get '/' do
