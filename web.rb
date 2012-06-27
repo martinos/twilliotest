c76b4cb630d5a5f7d2421f0b7305cab80f5dea6d
Martin Chabot <chabotm@gmail.com>
2012-06-27 03:30:06 -0400

    Removing crap

--- web.rb
+++ web.rb
@@ -14,9 +14,6 @@ post '/' do
 #  end
   response = Twilio::TwiML::Response.new do |r|
     r.Say 'Bonjour les amis', :voice => 'woman', :language => 'fr'
-    r.Dial :callerId => '+15147560096' do |d|
-      d.Client 'jenny'
-    end
   end
   response.text
 end
