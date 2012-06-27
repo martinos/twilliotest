65911625ea6302a0884711aa63e85073abbe85a3
Martin Chabot <chabotm@gmail.com>
2012-06-27 16:07:38 -0400

    Point

--- web.rb
+++ web.rb
@@ -19,7 +19,7 @@ post '/' do
     r.Say 'Bonjour les amis!', :voice => 'woman', :language => 'fr'
     r.Gather :action => "/gather", :numDigits => 1  do |gather|
       gather.Say "Si vous êtes Stéphane Hamel appuyez sur 1.", :voice => 'woman', :language => 'fr'
-      gather.Say "Si vous êtes Étienne Savard sur 2.", :voice => 'woman', :language => 'fr'
+      gather.Say "Si vous êtes Étienne Savard sur 2. ", :voice => 'woman', :language => 'fr'
     end
   end
   response
