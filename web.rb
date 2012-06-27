896d92064d378ab922635601dab7942a23e158b2
Martin Chabot <chabotm@gmail.com>
2012-06-27 02:40:15 -0400

    This is a test

--- web.rb
+++ web.rb
@@ -3,12 +3,19 @@ require "bundler/setup"
 
 require 'sinatra'
 require 'builder'
+require 'twillio-ruby'
 
 post '/' do
-  builder do |xml|
-    xml.instruct!
-    xml.Response do |xml|
-      xml.Say "Your account is live. Make a call to your Sandbox Number and test our demo Twilio app. Call and, when prompted, enter your pin" 
+#  builder do |xml|
+#    xml.instruct!
+#    xml.Response do |xml|
+#      xml.Say "Your account is live. Make a call to your Sandbox Number and test our demo Twilio app. Call and, when prompted, enter your pin" 
+#    end
+#  end
+  response = Twilio::TwiML::Response.new do |r|
+    r.Say 'hello there', :voice => 'woman'
+    r.Dial :callerId => '+15147560096' do |d|
+      d.Client 'jenny'
     end
   end
 end
