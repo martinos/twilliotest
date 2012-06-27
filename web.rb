aba60bc91d698b824db46fdb84243cc5d0c9c8eb
Martin Chabot <chabotm@gmail.com>
2012-06-27 11:56:38 -0400

    Changing welcome number

--- web.rb
+++ web.rb
@@ -8,16 +8,10 @@ require 'yaml'
 
 $stdout.sync = true
 post '/' do
-#  builder do |xml|
-#    xml.instruct!
-#    xml.Response do |xml|
-#      xml.Say "Your account is live. Make a call to your Sandbox Number and test our demo Twilio app. Call and, when prompted, enter your pin" 
-#    end
-#  end
   response = Twilio::TwiML::Response.new do |r|
     r.Say 'Bonjour les amis!', :voice => 'woman', :language => 'fr'
     r.Gather :action => "/gather" do |gather|
-      gather.Say "Entrez un nombre.", :voice => 'woman', :language => 'fr'
+      gather.Say "Entrez un nombre suivit du diese.", :voice => 'woman', :language => 'fr'
     end
   end
   puts request.params.to_yaml
