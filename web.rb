b976af789b031df95d4dc60272fbf4f51f564e09
Martin Chabot <chabotm@gmail.com>
2012-06-27 02:29:51 -0400

    Changing welcome message

--- web.rb
+++ web.rb
@@ -8,7 +8,7 @@ post '/' do
   builder do |xml|
     xml.instruct!
     xml.Response do |xml|
-      xml.Say "This is a test" 
+      xml.Say "Your account is live. Make a call to your Sandbox Number and test our demo Twilio app. Call and, when prompted, enter your pin" 
     end
   end
 end
